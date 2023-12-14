2f5f4653be863d86f6d6341d8567a58dca3208f8
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2023-12-14 19:22:10 +0000

    add short documentation for data fetching with axios on the client index page

--- src/index.js
+++ src/index.js
@@ -1,14 +1,14 @@
 const axios = require('axios')
+const Guest = require('./guest')
 
-console.log('still nothing inside')
-
-// fetch guests from a localhost server endpoint at route 'guests' with axios
+// fetch guests from a localhost server endpoint at route different route, 'guests' with axios, with a Promise (but no async function as of now)
+// This brings in the raw data (an object array) from the get method defined at that other route, to log it into the console
 axios.get('http://localhost:3000/guests').then(res => console.log(res.data))
 
-// create a guest with axios
-axios
+// create a guest with axios (fails, as of now)
+/* axios
   .post('http://localhost:3000/guests', { name: 'Gianni' })
   .then(response => {
     console.log(response.data)
   })
-  .catch(e => console.log(e.message))
+  .catch(e => console.log(e.message)) */
