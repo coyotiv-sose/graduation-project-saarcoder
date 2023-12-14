086487212f481b31c83d3b1287347bffc9d182c0
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2023-12-14 20:45:34 +0000

    clean up main function to get rid of logs

--- src/index.js
+++ src/index.js
@@ -2,23 +2,13 @@ const axios = require('axios')
 
 // fetch guests from a localhost server endpoint at route different route, 'guests' with axios, with a Promise (but no async function as of now)
 // This brings in the raw data (an object array) from the get method defined at that other route, to log it into the console
-axios.get('http://localhost:3000/guests').then(res => console.log(res.data))
+// axios.get('http://localhost:3000/guests').then(res => console.log(res.data))
 
 // create a new guest through HTTP API with axios
 async function main() {
-  const gianni = await axios
-    .post('http://localhost:3000/guests', { name: 'Gianni' })
-    .then(response => {
-      console.log(response.data)
-    })
-    .catch(e => console.log(e.message))
+  await axios.post('http://localhost:3000/guests', { name: 'Gianni' })
 
-  const tosh = await axios
-    .post('http://localhost:3000/guests', { name: 'Tosh' })
-    .then(response => {
-      console.log(response.data)
-    })
-    .catch(e => console.log(e.message))
+  await axios.post('http://localhost:3000/guests', { name: 'Tosh' })
 
   const allGuests = await axios.get('http://localhost:3000/guests')
   console.log('List of all users', allGuests.data)
