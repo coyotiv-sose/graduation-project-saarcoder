51e08cd2bae2fe962261767a2952808796cd0697
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-01-12 08:00:34 +0000

    Test if db has three users

--- tests/api.test.js
+++ tests/api.test.js
@@ -1,8 +1,13 @@
 const request = require('supertest')
 const app = require('../src/app')
 
-test('Return all users from my app successfully', async () => {
+test('Return first user from my app successfully', async () => {
   const response = await request(app).get('/users').expect(200)
   console.log(response.body)
   expect(response.body[0].firstName).toEqual('Trish')
+})
+test('Return all three users from my app successfully', async () => {
+  const response = await request(app).get('/users').expect(200)
+  console.log(response.body)
+  expect(response.body.length).toEqual(3)
 })
