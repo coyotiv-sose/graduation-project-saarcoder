29ed0c487d8e7d847d0bf29333c7aec641034388
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-01-19 08:42:04 +0000

    Test external api integration with OpenAI

---
+++ backend/openAI.js
@@ -0,0 +1,16 @@
+const OpenAI = require('openai')
+const dotenv = require('dotenv').config()
+
+const openai = new OpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+})
+
+async function main() {
+  const completion = await openai.chat.completions.create({
+    messages: [{ role: 'user', content: 'What is backtracking in computer science?' }],
+    model: 'gpt-3.5-turbo',
+  })
+
+  console.log(completion.choices[0])
+}
+main()
