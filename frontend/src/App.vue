1f0c5e0a5297806d14c80b3f87e425a50757f6ae
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-02-06 18:40:32 +0000

    Move user retrieval from Vue component attribute to HTML tag
    Co-authored-by: Numan Selman Duman <nsdumanlife@users.noreply.github.com>

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -27,8 +27,8 @@ export default {
     <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
 
     <div class="wrapper">
-      <HelloWorld :msg="user" />
-
+      <HelloWorld />
+      <p>{{ user }}</p>
       <nav>
         <RouterLink to="/">Home</RouterLink>
         <RouterLink to="/about">About</RouterLink>
