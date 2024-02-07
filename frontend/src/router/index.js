1675dea0443183c82c87cd0ef94a685709a77d9a
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-02-07 17:08:00 +0000

    Clean up logout route

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -10,17 +10,22 @@ const router = createRouter({
       component: HomeView
     },
     {
-      path: '/about',
-      name: 'about',
+      path: '/account',
+      name: 'your-account',
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
+      component: () => import('../views/AccountView.vue')
     },
     {
       path: '/login',
       name: 'login',
       component: () => import('../views/LoginView.vue')
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: () => import('../views/RegisterView.vue')
     }
   ]
 })
