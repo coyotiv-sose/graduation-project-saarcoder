4a61d1b15a103406489230396329430829ed9eb3
Alex Kimoto <66242241+saarcoder@users.noreply.github.com>
2024-02-17 18:27:48 +0000

    Obey naming convention

--- frontend/src/views/RegisterView.vue
+++ frontend/src/views/RegisterView.vue
@@ -1,26 +1,26 @@
 <script>
-import axios from 'axios'
-import { useAuthenticationStore } from '../stores/authenticationStore'
-import { mapActions } from 'pinia'
+  import axios from 'axios'
+  import { useAuthenticationStore } from '../stores/authentication'
+  import { mapActions } from 'pinia'
 
-export default {
-  name: 'RegisterView',
-  components: {},
-  data() {
-    return {
-      email: '',
-      password: '',
-      nickName: '',
-    }
-  },
-  methods: {
-    ...mapActions(useAuthenticationStore, ['register']),
-    async registerReroute() {
-      await this.register(this.email, this.nickName, this.password)
-      this.$router.push({ name: 'login' })
+  export default {
+    name: 'RegisterView',
+    components: {},
+    data() {
+      return {
+        email: '',
+        password: '',
+        nickName: '',
+      }
     },
+    methods: {
+      ...mapActions(useAuthenticationStore, ['register']),
+      async registerReroute() {
+        await this.register(this.email, this.nickName, this.password)
+        this.$router.push({ name: 'login' })
+      },
+    }
   }
-}
 </script>
 <template>
   <div>
